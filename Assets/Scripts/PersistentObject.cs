d1dbbde5b859c83f9cd8e5c8098d2e9197c45db5
Grimmy-hilde <gamsriwan@gmail.com>
2026-04-17 16:28:19 +0700

    LC

--- Assets/Scripts/PersistentObject.cs
+++ Assets/Scripts/PersistentObject.cs
@@ -9,9 +9,18 @@ public class PersistentObject : MonoBehaviour
     private string instancePrivateDebugText = "instance private";
     public string instancePublicDebugText = "instance public";
 
+    public static PersistentObject staticInstance = null;
+
     void Awake()
     {
+        if ( staticInstance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+              
+        }
         DontDestroyOnLoad(gameObject);
+        staticInstance = this;
     }
 
     void Start()
@@ -19,6 +28,8 @@ public class PersistentObject : MonoBehaviour
         staticPublicDebugText = "Hello (public)";
         staticPrivateDebugText = "Hello (private)";
         StartCoroutine(Loop());
+
+        GameSettings.volume = 1;
     }
 
     void Update()
@@ -54,6 +65,7 @@ public class PersistentObject : MonoBehaviour
     public void SetInstancePrivateText(string text)
     {
         instancePrivateDebugText = text;
+        staticPrivateDebugText = "XXX";
     }
 
 }
